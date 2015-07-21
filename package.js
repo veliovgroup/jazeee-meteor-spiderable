d19029a1cb8632947be45f82442c45cc8be2ad71
Dmitriy A. Golev <dr.dimitru@gmail.com>
2015-07-22 02:23:13 +0300

    Package desc

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "jazeee:spiderable-longer-timeout",
-  summary: "Makes the application crawlable to web spiders, longer timeout, no stdin issues, and a publish flag.",
+  summary: "Extended spiderable package with caching, longer timeout, no stdin issues, and a publish flag.",
   version: "1.2.0",
   git: "https://github.com/jazeee/jazeee-meteor-spiderable"
 });
