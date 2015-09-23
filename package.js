ce8ec77a679cb8770d913ad95f7b4b01a5de58ac
Enrico Foschi <foschi.enrico@gmail.com>
2015-09-23 07:48:12 +0200

    Package update for Meteor 1.2

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Package.onUse(function (api) {
   api.addFiles('lib/spiderable.coffee', ['client', 'server']);
   api.addFiles(['lib/spiderable.html', 'lib/client.coffee'], 'client');
   api.addFiles('lib/server.coffee', 'server');
-  api.addFiles('lib/phantom_script.js', 'server', { isAsset: true });
+  api.addAssets('lib/phantom_script.js', 'server');
 
   api.export('Spiderable');
 });
