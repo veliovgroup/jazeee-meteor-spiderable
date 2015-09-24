8470a0442d9b7c5fda15ad498ac9301e780724f9
Enrico Foschi <foschi.enrico@gmail.com>
2015-09-24 23:33:47 +0200

    Version update

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "jazeee:spiderable-longer-timeout",
   summary: "Extended spiderable package: SSL, caching, longer timeout, no stdin issues, publish flag",
-  version: "1.2.8",
+  version: "1.2.9",
   git: "https://github.com/jazeee/jazeee-meteor-spiderable"
 });
 
