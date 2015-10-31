150a90b0e0a09575a873f83d321acbfb96741faa
Dmitriy A. Golev <dr.dimitru@gmail.com>
2015-10-31 19:21:34 +0300

    v1.2.12
     - Update dependency to `ostrio:meteor-root`

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "jazeee:spiderable-longer-timeout",
   summary: "Extended spiderable package: SSL, caching, longer timeout, no stdin issues, publish flag",
-  version: "1.2.11",
+  version: "1.2.12",
   git: "https://github.com/jazeee/jazeee-meteor-spiderable"
 });
 
 Package.onUse(function (api) {
   api.versionsFrom("METEOR@0.9.0");
-  api.use(['webapp', 'mongo@1.1.3', 'ostrio:meteor-root@1.0.0'], 'server');
+  api.use(['webapp', 'mongo@1.1.3', 'ostrio:meteor-root@1.0.2'], 'server');
   api.use(['templating'], 'client');
   api.use(['underscore', 'coffeescript'], ['client', 'server']);
 
